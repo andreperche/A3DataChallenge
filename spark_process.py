e4d2e97f14e427efb042e4dfd0d7afe1297eb344
andreperche <andre.a.perche@gmail.com>
2021-06-16 23:24:53 -0300

    Process v1.2

--- spark_process.py
+++ spark_process.py
@@ -9,33 +9,24 @@ Created on Tue Jun 15 19:52:26 2021
 #pip install pyspark
 #pip install pandas
 
+#Import pacjages 
 from datetime import datetime
+from pyspark.sql import SparkSession
+
 print('Job start: '+'{}'.format(datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
 
-##################################  #SETUP ####################################
 
-# from pyspark.conf import SparkConf
-# from pyspark.context import SparkContext
-# ##Set Spark parameters
-# """ !?WTF?! Once the physical memory is exhausted, swap will be used instead. 
-#             If you have enough swap configured, you will be able to make use of 16 GB. 
-#             But your process will most likely slow down to a crawl when it starts swapping.
-# """
-# conf = SparkConf().setMaster("local").setAppName("My application").set("spark.executor.memory", "16g")
-# sc = SparkContext(conf)
-
-
-##Start Spark Session
-from pyspark.sql import SparkSession
+## Start Spark Session
 spark = SparkSession.builder.appName("ProcessamentoA3Challenge") \
         .getOrCreate()
         # .master("local[1]")
        
 
-##Import packages
-print('Imports packages: '+'{}'.format(datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
-
-############################ #Loading and Framming ############################
-
-df = spark.read.options(header='True',inferSchema='True',delimiter=';').format("csv").load(r"data/2019/RAIS_VINC_PUB_CENTRO_OESTE.txt")
+## Loading Parquets 
+print('Imports parquets: '+'{}'.format(datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
+df = spark.read.parquet("2010/")
 df.printSchema()
+
+print('Imports Ok: '+'{}'.format(datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
+
+
