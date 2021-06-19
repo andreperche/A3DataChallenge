cfb890c8ff7f855b10edab5268e527b3195591c7
Davidson Queiroz <davidson645@gmail.com>
2021-06-19 00:33:46 -0300

    t

--- filter_parquet.py
+++ filter_parquet.py
@@ -28,30 +28,7 @@ for fo in folders:
             # Columns select and filter            
             if(f.upper()[:4] != "ESTB" and f.upper()[5:10] != "ESTAB"): 
                 print("|---File: ", f)                       
-                pf.append( pq.read_table(f
-                                    ,columns=[ 'Qtd Hora Contr'
-                                                ,'Idade'
-                                                ,'Faixa Tempo Emprego'
-                                                ,'Escolaridade após 2005'
-                                                ,'Faixa Etária'
-                                                ,'Vínculo Ativo 31/12'                                                
-                                                ,'CNAE 2.0 Classe'
-                                                ,'Sexo Trabalhador'
-                                                ,'Vl Remun Média Nom'
-                                                ,'Tipo Vínculo'
-                                            ]                                
-                                ))
-        arrow_table = pyarrow.concat_tables(pf)
-        pq.write_table(arrow_table,'../summary/'+fo+'.parquet')
-        os.chdir("../")
-
-# All years in one parquet file
-os.chdir(dirSummary)   
-pf = []
-files = glob.glob('*.parquet')        
-for f in files:
-    if(f != "summary.parquet"):
-        pf.append( pq.read_table(f
+                table = pq.read_table(f
                             ,columns=[ 'Qtd Hora Contr'
                                                 ,'Idade'
                                                 ,'Faixa Tempo Emprego'
@@ -63,10 +40,40 @@ for f in files:
                                                 ,'Vl Remun Média Nom'
                                                 ,'Tipo Vínculo'
                                             ]                                
-                                ))
+                                ) 
+                new_table = table.rename_columns(['qtd_hora','idade','faixa_tempo','escolaridade','faixa_etaria','vinculo','cnae','sexo','vl_remun','tipo_vinculo'])
+                pf.append(new_table)
+                
+        arrow_table = pyarrow.concat_tables(pf)
+        pq.write_table(arrow_table,'../summary/'+fo+'.parquet')
+        os.chdir("../")
+
+# All years in one parquet file
+'''
+os.chdir(dirSummary)   
+pf = []
+files = glob.glob('*.parquet')        
+for f in files:
+    if(f != "summary.parquet"):
+
+        table = pq.read_table(f
+                            ,columns=[ 'Qtd Hora Contr'
+                                                ,'Idade'
+                                                ,'Faixa Tempo Emprego'
+                                                ,'Escolaridade após 2005'
+                                                ,'Faixa Etária'
+                                                ,'Vínculo Ativo 31/12'                                                
+                                                ,'CNAE 2.0 Classe'
+                                                ,'Sexo Trabalhador'
+                                                ,'Vl Remun Média Nom'
+                                                ,'Tipo Vínculo'
+                                            ]                                
+                                ) 
+        new_table = table.rename_columns(['qtd_hora','idade','faixa_tempo','escolaridade','faixa_etaria','vinculo','cnae','sexo','vl_remun','tipo_vinculo'])
+        pf.append(new_table)
 arrow_table = pyarrow.concat_tables(pf)
 pq.write_table(arrow_table,'../summary/summary.parquet')         
-
+'''
 #pandas = pat.to_pandas()
 #print(pandas.head(25))
 
